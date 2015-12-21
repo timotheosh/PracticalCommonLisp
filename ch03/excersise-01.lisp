0765ee57474db14cfa7034b99c2c16d30c15234d
Tim Hawes <tim.hawes@inin.com>
2015-12-21 13:06:26 -0500

    Progress

--- ch03/excersise-01.lisp
+++ ch03/excersise-01.lisp
@@ -33,3 +33,8 @@
                        :if-exists :supersede)
     (with-standard-io-syntax
       (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
