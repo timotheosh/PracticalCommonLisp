723769646bfcf0c4abd4b4da5df820625607cc7e
Tim Hawes <tim@selfdidactic.com>
2016-01-28 22:11:07 -0500

    progress

--- ch09/exercises-09.lisp
+++ ch09/exercises-09.lisp
@@ -1,47 +1,83 @@
 
+(defvar *test-name* nil)
+
 (defmacro with-gensyms ((&rest names) &body body)
   `(let ,(loop for n in names collect `(,n (gensym)))
      ,@body))
 
-(defmacro check (form)
-  `(report-result ,form ',form))
+;(defmacro check (form)
+;  `(report-result ,form ',form))
+
+;(defmacro check (&body forms)
+;  `(progn
+;     ,@(loop for f in forms collect `(report-result ,f ',f))))
 
 (defmacro check (&body forms)
-  `(progn
+  `(combine-results
      ,@(loop for f in forms collect `(report-result ,f ',f))))
 
 (defmacro combine-results (&body forms)
   (with-gensyms (result)
     `(let ((,result t))
-      ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
-      ,result)))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+;(defun report-result (result form)
+;  (format t "~:[FAIL~;pass~] ... ~a~%" result form))
 
 (defun report-result (result form)
-  (format t "~:[FAIL~;pass~] ... ~a~%" result form))
+  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
+  result)
 
-(defun test-+ ()
-  (and
-    (= (+ 1 2) 3)
-    (= (+ 1 2 3) 6)
-    (= (+ -1 -3) -4)))
+;(defun test-+ ()
+;  (and
+;    (= (+ 1 2) 3)
+;    (= (+ 1 2 3) 6)
+;    (= (+ -1 -3) -4)))
 
-(defun test-+ ()
-  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ 1 2) 3) '(= (+ 1 2) 3))
-  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
-  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
+;(defun test-+ ()
+;  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ 1 2) 3) '(= (+ 1 2) 3))
+;  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
+;  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
 
-(defun test-+ ()
+(defmacro deftest (name parameters &body body)
+  `(defun ,name ,parameters
+    (let ((*test-name* (append *test-name* (list ',name))))
+      ,@body)))
+
+;(defun test-+ ()
+;  (let ((*test-name* "test-+"))
+;    (check
+;      (= (+ 1 2) 3)
+;      (= (+ 1 2 3) 6)
+;      (= (+ -1 -3) -4))))
+
+(deftest test-+ ()
   (check
     (= (+ 1 2) 3)
     (= (+ 1 2 3) 6)
     (= (+ -1 -3) -4)))
 
-(defun test-* ()
+;(defun test-* ()
+;  (let ((*test-name* "test-*"))
+;    (check
+;      (= (* 2 2) 4)
+;      (= (* 3 5) 15))))
+
+(deftest test-* ()
   (check
     (= (* 2 2) 4)
     (= (* 3 5) 15)))
 
-(defun test-arithmetic ()
+;(defun test-arithmetic ()
+;  (combine-results
+;   (test-+)
+;   (test-*)))
+
+(deftest test-arithmetic ()
   (combine-results
    (test-+)
    (test-*)))
+
+(deftest test-math ()
+  (test-arithmetic))
